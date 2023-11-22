52d92ef1a0e1eddf0ec1714258ca45a7884e7609
Yotsawat Sukinee <yotsawat2557@gmail.com>
2023-11-22 15:32:23 +0700

    submit lab01

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,32 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+import java.util.Scanner;
+import java.util.Arrays;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public class Main {
+    public static void Sortout(int[] ar,int n) {
+        Arrays.sort(ar);
+        System.out.println("Sorted result:");
+        for (int i = 0; i < n; i++) {
+            System.out.println((i+1)+") "+ar[i]);
         }
+    }
+    public static void main(String[] args)
+    {
+        Scanner mynum = new Scanner(System.in);
+        System.out.print("Enter amout of num: ");
+        int n = mynum.nextInt();
+        if (n < 0) {
+            System.out.println("Pls enter a non-negative int");
+            mynum.close();
+            return;
+        }
+        int[] numbers = new int[n];
+        System.out.println("Enter " + n + " integers");
+        for (int i = 0; i < n; i++) {
+            System.out.print((i+1)+") ");
+            numbers[i] = mynum.nextInt();
+        }
+        Sortout(numbers,n);
+        mynum.close();
+
     }
 }
